cd0cdcc7277db47037f9cb7790a50f02e400ecf6
fuatX <fuatyagizaydin@hotmail.com>
2023-07-25 19:48:24 +0300

    Quiz-app-started

---
+++ js/quiz.js
@@ -0,0 +1,63 @@
+// let Question = {
+//   questionText: "Hangisi Javascript paket yönetim uygulamasıdır?",
+//   answerOptions: {
+//     a: "Node.js",
+//     b: "Typescript",
+//     c: "Npm",
+//   },
+//   correctAnswer: "c",
+//   checkAnswer: function (answer) {
+//     return answer === this.correctAnswer;
+//   },
+// };
+
+function Question(questionText, answerOptions, correctAnswer) {
+  this.questionText = questionText;
+  this.answerOptions = answerOptions;
+  this.correctAnswer = correctAnswer;
+  //   this.checkAnswer = function (answer) {
+  //     return answer === this.correctAnswer;
+  //   };
+  //   console.log(this);
+}
+
+let question1 = new Question(
+  "Hangisi Javascript paket yönetim uygulamasıdır?",
+  { a: "Node.js", b: "Typescript", c: "Npm" },
+  "c"
+);
+
+Question.prototype.checkAnswer = function (answer) {
+  return answer === this.correctAnswer;
+};
+
+let questions = [
+  new Question("1-Hangisi js paket yönetim uygulamasıdır?"),
+  new Question("2-Hangisi js paket yönetim uygulamasıdır?"),
+  new Question("3-Hangisi js paket yönetim uygulamasıdır?"),
+  new Question("4-Hangisi js paket yönetim uygulamasıdır?"),
+];
+
+function Quiz(questions) {
+  this.questions = questions;
+  this.questionIndex = 0;
+  //question index değerini, question içine 1.ye 0 olarak 2.ye 1 olarak döndürürerek index değeri sağlar
+}
+
+const quiz = new Quiz(questions);
+
+Quiz.prototype.BringQuestion = function () {
+  return this.questions[this.questionIndex];
+};
+
+// quiz.questionIndex += 1;
+// console.log(quiz.BringQuestion());
+
+document.querySelector(".btn-start").addEventListener("click", function () {
+  if (quiz.questions.length != quiz.questionIndex) {
+    console.log(quiz.BringQuestion());
+    quiz.questionIndex += 1;
+  } else {
+    console.log("Quiz bitti");
+  }
+});
